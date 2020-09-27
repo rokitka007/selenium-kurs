ba9903016ed4eab96b0237aedbf4a4d3efcb9459
Uzytkownik <pgacek03@gmail.com>
2020-09-27 14:40:57 +0200

    Podział logiki biznesowej - testy

---
+++ src/com/selenium/pageObject/BookingResult.java
@@ -0,0 +1,47 @@
+package com.selenium.pageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class BookingResult {
+
+    WebDriver driver;
+
+    @FindBy(className = "sr_header")
+    WebElement header;
+
+    @FindBy(className = "js-coronavirus-banner")
+    WebElement coronavirusInfo;
+
+    @FindBy(className = "hotel_name_link")
+    WebElement hotelLinks;
+
+    public BookingResult(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(this.driver, this);
+    }
+
+    public Boolean checkHeader(String destination) {
+        if (header.getText().contains(destination)) {
+            return Boolean.TRUE;
+        } else {
+            return Boolean.FALSE;
+        }
+    }
+
+    public String getCoronavirusInfo() {
+        return coronavirusInfo.getText();
+    }
+
+    public void enterHotelLink(String hotelName) {
+        WebElement hotel = hotelLinks.findElement(By.xpath("//*[contains(text(), " + hotelName + ")]"));
+//        System.out.println(hotel.getText());
+        //WebElement hotel = hotelLinks.findElement(By.xpath(String.format("//*[contains(text(), "+ hotelName +")]")));
+        hotel.click();
+    }
+
+
+}
---
+++ src/com/selenium/pageObject/BookingTest.java
@@ -0,0 +1,44 @@
+package com.selenium.pageObject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class BookingTest {
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Ja\\Desktop\\Patrycja\\Dokumenty\\KURS\\Repo_se\\resources\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void checkHotelView() {
+        BookingHome home = new BookingHome(driver);
+        Assert.assertEquals(home.getSiteTitle(), "Booking.com | Oficjalna strona | Najlepsze hotele i nie tylko");
+        String destination = "Urowo";
+        String hotel = "Domek u Mira";
+//        home.searchDestination(String.format(destination + " " + hotel));
+        home.searchDestination(String.format("%s %s", destination, hotel));
+        BookingResult result = new BookingResult(driver);
+
+        Assert.assertTrue(result.checkHeader(destination));
+        Assert.assertEquals(result.getCoronavirusInfo(), "Koronawirus (COVID-19) – wsparcie");
+//        result.enterHotelLink(hotel);
+    }
+
+
+    @AfterTest
+    public void cleanUp() {
+        driver.quit();
+    }
+}
