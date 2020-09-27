d3b2dda3180ec11bb28019d31aee94691a5e1fea
Maciek P <maciek_p@interia.pl>
2020-09-27 12:47:27 +0200

    dodanie buttona i timeoutu w  testNG

--- src/com/selenium/testNG/BookingSite.java
+++ src/com/selenium/testNG/BookingSite.java
@@ -4,38 +4,59 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class BookingSite {
 
     WebDriver driver;
 
+    @FindBy(className = "sb-destination__input")
+    WebElement destinationInput;
+
+    @FindBy(className = "sb-searchbox__button")
+    WebElement searchButton;
+
     @BeforeTest
     public void setup() {
-        DesiredCapabilities caps = new DesiredCapabilities();
+//        DesiredCapabilities caps = new DesiredCapabilities();
         System.setProperty("webdriver.gecko.driver", "C:\\mahnamahna\\selenium-kurs\\resources\\geckodriver.exe");
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://booking.com");
+
+        PageFactory.initElements(driver, this);
     }
 
     @AfterTest
-    public void  cleaner(){
+    public void cleaner() {
         driver.quit();
     }
 
     @Test
-    public void checkDestinationPlaceholder(){
-        driver.get("https://booking.com");
-        WebElement destinationImput = driver.findElement(By.className("sb-destination__input"));
-        String placeholder = destinationImput.getAttribute("placeholder");
+    public void checkDestinationPlaceholder() {
+        String placeholder = destinationInput.getAttribute("placeholder");
         Assert.assertEquals(placeholder, "Where are you going?");
+    }
 
+    @Test
+    public void findDestination() throws Exception {
+        destinationInput.sendKeys("Zakopane");
+        searchButton.click();
+        driver.findElement(By.id("basiclayout"));
+        String newUrl = driver.getCurrentUrl();
+        Assert.assertTrue(newUrl.contains("searchresults"));
     }
 
 
 
 }
+
+
