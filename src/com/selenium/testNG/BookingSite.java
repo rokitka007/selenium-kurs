a96c9b9f335d9f1f80698539891e53db413224dd
Uzytkownik <pgacek03@gmail.com>
2020-09-27 13:21:18 +0200

    RentCar Link Test

--- src/com/selenium/testNG/BookingSite.java
+++ src/com/selenium/testNG/BookingSite.java
@@ -5,37 +5,67 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class BookingSite {
+    //
     WebDriver driver;
 
+    @FindBy(className = "sb-destination__input")
+    WebElement destinationInput;
+
+    @FindBy(className = "sb-searchbox__button")
+    WebElement searchButton;
+
+    @FindBy(xpath = "//a[@data-ga-track='click|Product Expansion|cars|rentalcars (index)']")
+    WebElement rentCarLink;
 
     @BeforeTest
-    public void setup(){
+    public void setup() {
         DesiredCapabilities caps = new DesiredCapabilities();
         System.setProperty("webdriver.gecko.driver", "C:\\Users\\Ja\\Desktop\\Patrycja\\Dokumenty\\KURS\\Repo_se\\resources\\geckodriver.exe");
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://booking.com");
+        //Inicjalizujemy wszystkie elemnty strony opisane w atrybutach klasy
+        PageFactory.initElements(driver, this);
 
     }
 
     @Test
-    public void checkDestinationPlaceholder(){
-        driver.get("https://booking.com");
-        WebElement destinationInput =driver.findElement(By.className("sb-destination__input"));
+    public void checkDestinationPlaceholder() {
+
         String placeholder = destinationInput.getAttribute("placeholder");
-        Assert.assertEquals(placeholder,"Dokąd się wybierasz?");
+        Assert.assertEquals(placeholder, "Dokąd się wybierasz?");
 
     }
 
+    @Test
+    public void findDestination() throws Exception {
+        destinationInput.sendKeys("Zakopane");
+        searchButton.click();
+        driver.findElement(By.id("basiclayout"));
+        String newUrl = driver.getCurrentUrl();
+        Assert.assertTrue(newUrl.contains("searchresult"));
+    }
 
+    @Test
+    public void renCars() {
+        rentCarLink.click();
+        String newUrl = driver.getCurrentUrl();
+        Assert.assertTrue(newUrl.contains("cars"));
+    }
 
     @AfterTest
-    public void  cleanUp(){
+    public void cleanUp() {
         driver.quit();
     }
 }
