d60364e554c6d22b7c0da6566e1f9cbafd31be97
BarbaraKocur <barrrbarosssa@gmail.com>
2020-09-27 13:18:55 +0200

    RentCar Link Test

--- src/com/selenium/testNG/BookingSite.java
+++ src/com/selenium/testNG/BookingSite.java
@@ -5,13 +5,27 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class BookingSite {
     WebDriver driver;
+
+    @FindBy(className = "sb-searchbox__button")
+
+    WebElement searchButton;
+
+    @FindBy(className = "sb-destination__input")
+    WebElement destinationInput;
+
+    @FindBy(xpath = "//a[@data-ga-track=\"click|Product Expansion|cars|rentalcars (index)\"]")
+    WebElement searchLink;
 
 
     @BeforeTest
@@ -20,15 +34,38 @@ public class BookingSite {
         System.setProperty("webdriver.gecko.driver", "C:\\Users\\Basia\\Desktop\\liveTesting BDD\\selenium-kurs\\resources\\geckodriver.exe");
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://booking.com");
+
+        //Inicjalizujemy wszystkie elementy ze strony opisane w atrybutach klasy
+        PageFactory.initElements(driver,this);
 
     }
     @Test
     public void checkDestinationPlaceholder(){
-        driver.get("https://booking.com");
-        WebElement destinationInput = driver.findElement(By.className("sb-destination__input"));
         String placeholder = destinationInput.getAttribute("placeholder");
         Assert.assertEquals(placeholder, "Dokąd się wybierasz?");
     }
+
+    @Test
+    public void findDestination(){
+        destinationInput.sendKeys("Zakopane");
+        searchButton.click();
+        driver.findElement(By.id("basiclayout"));
+        String newUrl = driver.getCurrentUrl();
+        Assert.assertTrue(newUrl.contains("searchresults"));
+    }
+
+
+    @Test
+    public void rentCar(){
+        searchLink.click();
+        String newUrl = driver.getCurrentUrl();
+        Assert.assertTrue(newUrl.contains("cars"));
+
+    }
+
+
 
     @AfterTest
     public void cleanUp(){
