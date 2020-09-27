d1eb8e5450c35b78558460a997849fb0afb94243
rokitka007 <kattie_rokitka@wp.pl>
2020-09-27 10:02:55 +0200

    Initial test - podstawy selenium

---
+++ src/com/selenium/podstawy/Main.java
@@ -0,0 +1,31 @@
+package com.selenium.podstawy;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hej selenium");
+
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\kpopiela\\IdeaProjects\\selenium-kurs\\resources\\geckodriver.exe");
+        // Uruchomienie silnika przeglądarki
+        WebDriver driver = new FirefoxDriver();
+
+        // Wejście na stronę (URL)
+        driver.get("https://booking.com");
+
+        //Informacje o stronie
+        System.out.println(driver.getTitle());
+
+        //Deklaracja elementu strony
+        WebElement element;
+        element = driver.findElement(By.className("sb-destination__input"));
+        System.out.println(element.getAttribute("placeholder"));
+
+        //Wyjście z przeglądarki
+        driver.quit();
+    }
+}
